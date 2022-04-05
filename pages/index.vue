b1550f8e1ae7523759d34f1292f93994e0257a16
Dominik Szojda <szojda.98@gmail.com>
2022-04-05 16:59:51 +0200

    main page

--- pages/index.vue
+++ pages/index.vue
@@ -24,11 +24,21 @@
     <section class="podStrony">
       <div class="podStrony_box">
         <h4>psitinder</h4>
-        <p>jakis opis</p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+        </p>
+        <NuxtLink to="/dodaj">więcej</NuxtLink>
       </div>
       <div class="podStrony_box rasy">
         <h4>wszystie rasy</h4>
-        <p>jakis opis</p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+        </p>
+        <NuxtLink to="/przeglondaj">więcej</NuxtLink>
       </div>
       <div class="blueCircle"></div>
     </section>
@@ -47,8 +57,16 @@
           including versions of Lorem Ipsum.
         </p>
       </div>
-      <div class="boxColor"></div>
     </section>
+    <div class="boxColor">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#00cba9"
+          fill-opacity="1"
+          d="M0,64L48,80C96,96,192,128,288,138.7C384,149,480,139,576,128C672,117,768,107,864,90.7C960,75,1056,53,1152,80C1248,107,1344,181,1392,218.7L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -75,13 +93,22 @@ div {
 }
 .title h1 {
   font-size: 8em;
+  background: linear-gradient(
+    53deg,
+    rgb(19, 255, 98) 0%,
+    rgba(83, 83, 83, 1) 46%,
+    #0c7fcb 100%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
 }
 .title h2 {
   font-size: 3em;
 }
 .greenCircle {
   position: absolute;
-  background-color: rgb(80, 238, 80);
+  background-color: #00cba9;
   padding: 300px;
   z-index: -1;
   border-radius: 50%;
@@ -110,7 +137,7 @@ div {
 }
 .obrazek h3 {
   position: absolute;
-  color: rgb(68, 71, 238);
+  color: #0099ff;
   bottom: 5%;
   right: 5%;
   transform: rotate(-25deg);
@@ -126,21 +153,83 @@ img:hover {
   display: flex;
   justify-content: space-evenly;
   margin-top: 5rem;
+  min-height: 50vh;
 }
 .podStrony_box {
-  width: 30%;
+  width: 20%;
+  height: 200px;
   box-shadow: 2px 2px 30px 10px rgba(0, 0, 0, 0.5);
   background-color: whitesmoke;
+  z-index: 1;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  padding: 20px;
+  overflow: hidden;
+  transition: 0.7s;
+}
+.podStrony_box:after {
+  content: " ";
+  transition: 0.7s;
+  width: 100%;
+  height: 80%;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  background: linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 0.795) 30%,
+    rgba(255, 255, 255, 0) 100%
+  );
+}
+.podStrony_box:hover:after {
+  opacity: 0;
+}
+.podStrony_box:hover {
+  height: 300px;
+}
+.podStrony_box h4 {
+  margin-top: 4rem;
+  font-size: 2.2em;
+  text-align: center;
+}
+.podStrony_box a {
+  text-align: left;
+  color: black;
   z-index: 1;
 }
 .blueCircle {
   position: absolute;
-  background-color: rgb(70, 29, 219);
+  background-color: #0099ff;
   padding: 300px;
   z-index: -2;
   border-radius: 50%;
   left: -10%;
   top: -10%;
+}
+.quiz {
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  width: 70%;
+  justify-content: center;
+  align-content: center;
+  margin: 0 auto;
+  text-align: center;
+}
+.quiz h2 {
+  font-size: 4em;
+}
+.quiz p {
+  font-size: 1.5em;
+}
+.boxColor {
+  position: relative;
+  left: -2%;
+  bottom: -2%;
+  width: 120%;
+  height: 50%;
 }
 
 @keyframes outlineIn {
