2d30e76bcc2efb3a6d5ed42da30b2b4e9a40d592
Dominik Szojda <szojda.98@gmail.com>
2022-04-07 10:35:52 +0200

    quiz logika V1

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -25,21 +25,38 @@
           <p>{{ questions[currentQuestion].pytanie }}</p>
           <span class="answer">
             <label
-              ><input name="answer" type="radio" value="A: " />A:
-              {{ questions[currentQuestion].odpA }}</label
+              ><input
+                name="answer"
+                v-model="an"
+                type="radio"
+                :value="questions[currentQuestion].odpA.val"
+              />A: {{ questions[currentQuestion].odpA.answer }}</label
             >
             <label>
-              <input name="answer" type="radio" value="B: " />B:
-              {{ questions[currentQuestion].odpB }}
+              <input
+                name="answer"
+                v-model="an"
+                type="radio"
+                :value="questions[currentQuestion].odpB.val"
+              />B:
+              {{ questions[currentQuestion].odpB.answer }}
             </label>
             <label>
-              <input name="answer" type="radio" value="C: " />C:
-              {{ questions[currentQuestion].odpC }}
+              <input
+                name="answer"
+                v-model="an"
+                type="radio"
+                :value="questions[currentQuestion].odpC.val"
+              />C:
+              {{ questions[currentQuestion].odpC.answer }}
             </label>
-            <label v-if="questions[currentQuestion].odpD != ''">
-              <input name="answer" type="radio" value="D: " />D:{{
-                questions[currentQuestion].odpD
-              }}
+            <label v-if="questions[currentQuestion].odpD.answer != ''">
+              <input
+                name="answer"
+                v-model="an"
+                type="radio"
+                :value="questions[currentQuestion].odpD.val"
+              />D:{{ questions[currentQuestion].odpD.answer }}
             </label>
           </span>
         </section>
@@ -47,7 +64,7 @@
           <button v-if="currentQuestion < 3" @click="nextQuetion">
             następne
           </button>
-          <button v-else>podsumowanie</button>
+          <button v-else @click="findDong">podsumowanie</button>
         </section>
       </div>
     </section>
@@ -58,41 +75,142 @@ export default {
   data() {
     return {
       currentQuestion: 0,
+      an: "",
+      tableAns: [],
+      dogsTab: [],
       questions: [
         {
           pytanie: "wielkość psa",
-          odpA: "Duży",
-          odpB: "średni",
-          odpC: "mały",
-          odpD: "",
+          odpA: {
+            answer: "Duży",
+            val: "40",
+          },
+          odpB: {
+            answer: "Średni",
+            val: "20-30",
+          },
+          odpC: {
+            answer: "Mały",
+            val: "20",
+          },
+          odpD: {
+            answer: "",
+            val: "50",
+          },
         },
         {
           pytanie: "Pies stworzony do",
-          odpA: "Obrony",
-          odpB: "Tropienia",
-          odpC: "Rodziny",
-          odpD: "Zawodów",
+          odpA: {
+            answer: "Obrony",
+            val: "guardian",
+          },
+          odpB: {
+            answer: "Tropienia",
+            val: "hunting",
+          },
+          odpC: {
+            answer: "Rodziny",
+            val: "Companionship",
+          },
+          odpD: {
+            answer: "Ratowniczy",
+            val: "Trailing",
+          },
         },
         {
           pytanie: "Usposobienie psa",
-          odpA: "Szcześliwy",
-          odpB: "Inteligentny",
-          odpC: "Lojalny",
-          odpD: "Aktywny",
+          odpA: {
+            answer: "Szcześliwy",
+            val: "Happy",
+          },
+          odpB: {
+            answer: "Inteligentny",
+            val: "Intelligent",
+          },
+          odpC: {
+            answer: "Lojalny",
+            val: "Loyal",
+          },
+          odpD: {
+            answer: "Aktywny",
+            val: "Active",
+          },
         },
         {
           pytanie: "długość życias",
-          odpA: "mniej niż 10lat",
-          odpB: "10-12",
-          odpC: "12-14",
-          odpD: "wiecej niż 14",
+          odpA: {
+            answer: "mniej niż 10lat",
+            val: "10",
+          },
+          odpB: {
+            answer: "10-12",
+            val: "10-12",
+          },
+          odpC: {
+            answer: "12-14",
+            val: "12-14",
+          },
+          odpD: {
+            answer: "wiecej niż 14",
+            val: "14",
+          },
         },
       ],
     };
   },
+  async asyncData() {
+    const dogsAsync = await fetch("https://api.thedogapi.com/v1/breeds", {
+      headers: {
+        "X-API-KEY": "b0a7328e-6b71-4693-a05d-b24a612d40e9",
+      },
+    }).then((res) => {
+      return res.json();
+    });
+    return { dogsAsync };
+  },
   methods: {
     nextQuetion() {
-      this.currentQuestion = this.currentQuestion + 1;
+      if (this.currentQuestion < 3)
+        this.currentQuestion = this.currentQuestion + 1;
+      this.tableAns.push(this.an);
+    },
+    findDong() {
+      this.tableAns.push(this.an);
+      const dogs = this.dogsAsync;
+      dogs.forEach((item) => {
+        if (
+          (item.height.metric.includes(this.tableAns[0]) &&
+            item.height.metric != undefined) ||
+          (item.bred_for.includes(this.tableAns[1]) &&
+            item.bred_for != undefined) ||
+          (item.temperament.includes(this.tableAns[2]) &&
+            item.temperament != undefined) ||
+          (item.life_span.includes(this.tableAns[3]) &&
+            item.life_span != undefined)
+        ) {
+          const dog = {
+            id: item.id,
+            breed_group: item.breed_group,
+            height: item.height.metric,
+            weight: item.weight.metric,
+            bred_for: item.bred_for,
+            name: item.name,
+            life_span: item.life_span,
+            temperament: item.temperament,
+            image: item.image.url,
+            name: item.name,
+          };
+          this.dogsTab.push(dog);
+        }
+
+        console.log(this.dogsTab);
+      });
+      // if(element > )
+
+      //      Guarding    Happy         <10
+      // smal  <30      hunting   Intelligent   10- 12
+      // medium  <40     Family   Loyal         12 -14
+      // big  >40       search    Active        >14
     },
   },
 };
@@ -155,7 +273,7 @@ input:hover {
 }
 .box__quiz--footer {
   display: flex;
-  justify-content: end;
+  justify-content: flex-end;
 }
 .blue {
   border-color: rgb(28, 3, 255) !important;
