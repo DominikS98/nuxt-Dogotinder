a6928b6db8df187a9c0961ed43330ccd23e6852c
Dominik Szojda <szojda.98@gmail.com>
2022-05-30 13:53:11 +0200

    mobile menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,20 +1,40 @@
 <template>
-  <ul>
-    <li>
-      <NuxtLink to="/psiTinder">Psitinder </NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/przeglondaj">Zobacz rasy </NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/">Strona główna</NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/quiz">Quiz</NuxtLink>
-    </li>
-  </ul>
+  <nav>
+    <ul class="menu" v-bind:class="{ OpenMenu: menuOn }">
+      <li>
+        <NuxtLink to="/psiTinder">Psitinder </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/przeglondaj">Zobacz rasy </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">Strona główna</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/quiz">Quiz</NuxtLink>
+      </li>
+    </ul>
+    <span @click="openMenu" class="hamburger">
+      <div></div>
+      <div></div>
+      <div></div>
+    </span>
+  </nav>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      menuOn: false,
+    };
+  },
+  methods: {
+    openMenu() {
+      this.menuOn = !this.menuOn;
+    },
+  },
+};
+</script>
 <style scoped>
 a.nuxt-link-active {
   font-weight: bold;
@@ -43,6 +63,30 @@ a {
   font-size: 1.5rem;
   font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 }
+.hamburger {
+  display: none;
+}
 @media only screen and (max-width: 600px) {
+  ul {
+    transition: 0.5s;
+    flex-flow: column;
+    transform: translateY(-200px);
+  }
+  .hamburger {
+    display: block;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+    z-index: 10;
+  }
+  .hamburger div {
+    border: 1px solid black;
+    padding: 0px 10px;
+    margin: 4px;
+  }
+  .OpenMenu {
+    transform: translateY(0px);
+  }
 }
 </style>
